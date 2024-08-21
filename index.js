c7c2f56e798c290bef1f6e97c14b97a08ac7c726
dhinaashwin <dhinaashwin11@gmail.com>
2024-08-22 02:05:42 +0530

    new

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 require('dotenv').config(); 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({
+  origin:['https://mugilherbals.vercel.app'],  
+  methods:['POST','GET'],
+  credentials:true }));
 app.use(express.json());
 const allowCors = fn => async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true)

600a433cda9ea0e5f22eef32d88e75910ba7a1ad
dhinaashwin <dhinaashwin11@gmail.com>
2024-08-22 08:39:18 +0530

    new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors({
-  origin:['*'],  
+  origin:['http://localhost:5173'],
   methods:['POST','GET'],
   credentials:true }));
 app.use(express.json());
